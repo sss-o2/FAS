eefe54f034d34166621691b92811e547032217f3
“sss-o2” <your@example.com>
2020-06-11 14:56:18 +0900

    add post create

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'bullet'
   gem 'rubocop-airbnb'
+  gem 'pry-byebug'
   
 end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,22 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+    #binding.pry
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to posts_path,notice: '投稿しました'
+    else
+      render :new
+    end
   end
 
   def edit
