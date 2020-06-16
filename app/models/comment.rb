78a6c8963f641ebe7b5c107cce0676df5d67b168
“sss-o2” <your@example.com>
2020-06-16 16:08:30 +0900

    add comment new,edit to image

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  has_many :post_images, dependent: :destroy
+  has_many :post_images, dependent: :destroy, foreign_key:'post_id'
   attachment :image
-
+  accepts_attachments_for :post_images, attachment: :image
 end
