3b23d0a5973d9eb954db75cc78d0e3b9c10f6e07
“sss-o2” <your@example.com>
2020-06-23 13:10:10 +0900

    gmailへの手動送信一式完成

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,8 +2,8 @@ class InquiryMailer < ApplicationMailer
   def send_mail(inquiry)
     @inquiry = inquiry
     mail(
-      from: 'system@example.com',
-      to:   'manager@example.com',
+      from: 'o.matsu.youser12@gmail.com',
+      to:   'fas.appadm@gmail.com',
       subject: 'お問い合わせ通知'
     )
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,14 @@ Rails.application.configure do
     Bullet.console = true
     Bullet.rails_logger = true
   end
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:            'fas.appadm@gmail.com',
+  password:             'aiueoaiu2',
+  authentication:       'plain',
+  enable_starttls_auto:  true
+  }
 end
