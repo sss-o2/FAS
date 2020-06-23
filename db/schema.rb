d92411312af4350f9a7f02ae17fee999c0e8be9c
“sss-o2” <your@example.com>
2020-06-23 09:58:17 +0900

    プレビュー画面表示まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_144911) do
+ActiveRecord::Schema.define(version: 2020_06_23_003418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2020_06_22_144911) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "comment_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
