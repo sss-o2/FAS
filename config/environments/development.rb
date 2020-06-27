a1af84bcdf6400dd94e204392b19681ad9684abc
“sss-o2” <your@example.com>
2020-06-27 21:20:46 +0900

    configのメール送信関係修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,12 +71,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                  587,
-  domain:               'gmail.com',
-  user_name:            ENV['MAIL_ADDRESS'],
-  password:             ENV['MAIL_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto:  true
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV['MAIL_ADDRESS'],
+    password:             ENV['MAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
   }
 end
