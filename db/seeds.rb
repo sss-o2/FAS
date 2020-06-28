cae5f7f4eecae0affec7e6c75f6a330720e77d3c
“sss-o2” <your@example.com>
2020-06-28 19:58:49 +0900

    シードファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,1 @@
 User.create(email: 'admin@test.com', name:'admin',password: 'password',admin: true)
-User.create(email: 'satou@test.com', name:'satou',password: 'password')
-User.create(email: 'suzuki@test.com',name:'suzuki', password: 'password')
-User.create(email: 'tanaka@test.com',name:'tanaka', password: 'password')
-User.create(email: 'test@test.com', name:'test',password: 'password')
