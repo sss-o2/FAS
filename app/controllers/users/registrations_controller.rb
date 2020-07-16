fdfdf6a6676cb7186c4026e56f11c0dc13b87848
“sss-o2” <your@example.com>
2020-07-16 14:33:25 +0900

    一時的にパスワードなし編集実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,7 +40,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # # protected
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
+  end
 
   # # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -67,4 +71,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def user_params
   #   params.permit(:sign_up, keys: [:email,:encrypted_password,:name])
   # end
+
 end
