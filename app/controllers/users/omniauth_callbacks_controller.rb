6b46120dca832bf25f00e36479940f74c2e43889
“sss-o2” <your@example.com>
2020-07-19 19:27:54 +0900

    google認証微修正（ユーザー名をモデルに記述）

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -34,9 +34,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    @user.update(name: request.env["omniauth.auth"].info.name)
+    #@user.update(name: request.env["omniauth.auth"].info.name)
 
     if @user.persisted?
+      #@user.update(name: request.env["omniauth.auth"].info.name)
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
