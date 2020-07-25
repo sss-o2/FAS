b090b1a473591fc630d0d23ae7f96d91ace79d7b
“sss-o2” <your@example.com>
2020-07-25 22:08:06 +0900

    お問い合わせフォームなどデザイン微修正

--- app/javascript/js_file/script.js
+++ app/javascript/js_file/script.js
@@ -1,8 +1,17 @@
-$(function() {
-  $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
-    event.preventDefault();
+$(function(){
+  var pagetop = $('#top a');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
   });
 });
