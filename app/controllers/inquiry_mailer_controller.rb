66001900ef9f6cc8cfc7ff9ad6e9cf9dae6e16b8
“sss-o2” <your@example.com>
2020-07-30 21:20:01 +0900

    コード修正

--- app/controllers/inquiry_mailer_controller.rb
+++ app/controllers/inquiry_mailer_controller.rb
@@ -3,23 +3,9 @@ class InquiryMailerController < ApplicationController
     @inquiry = Inquiry.new
   end
 
-  # def create
-  #   @inquiry = Inquiry.new(post_params)
-
-  #   if InquiryMailer.send_mail(@inquiry).deliver
-  #     redirect_to new_inquiry_mailer_path,notice: 'メール送信しました'
-  #   else
-  #     render :new
-  #   end
-  # end
-
   def confirm
     @inquiry = Inquiry.new(inquiry_params)
-    # if InquiryMailer.send_mail(@inquiry).deliver
-    #   redirect_to new_inquiry_mailer_path,notice: 'メール送信しました'
-    # else
-    #   render :confirm
-    # end
+
     if @inquiry.valid?
       render :action => 'confirm'
     else
@@ -38,13 +24,6 @@ class InquiryMailerController < ApplicationController
       end
     end
   end 
-
-  # def contact
-  #   @title = params[:title]
-  #   @message=params[:message]
-  #   InquiryMailer.test_mail(@title,@message).deliver
-  #   redirect_to action: :show,notice: 'メール送信しました'
-  # end
 
   def inquiry_params
     params.require(:inquiry).permit(:email,:name,:subject,:message)
